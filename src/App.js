2300047bd422d706e7fd4b5b2de448b4ca66c94c
sam-klein1 <sklein27@binghamton.edu>
2023-05-27 11:34:09 -0400

    most of the frontend finished, still need to finish title. Moving onto backend now anyways.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import GuessBoxes from './components/GuessBoxes';
+import GuessButton from './components/GuessButton';
+import CityImg from './components/CityImg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CapitLe</h1>
+      <CityImg />
+      <GuessBoxes />
+      {/* <GuessButton /> */}
     </div>
   );
 }
