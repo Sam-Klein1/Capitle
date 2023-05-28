10e6b30a1108e42a6cec1ec189b4ae8af36869c7
sam-klein1 <sklein27@binghamton.edu>
2023-05-28 10:14:00 -0400

    FRONTEND bascially finished now

--- src/components/GuessBoxes.jsx
+++ src/components/GuessBoxes.jsx
@@ -1,14 +1,68 @@
-import React from 'react';
-import '../css/GuessBoxes.css'
+import React, { useState, useEffect, useRef } from 'react';
+import { capitalCities } from '../cities.js';
+import '../css/GuessBoxes.css';
+import '../css/suggestions.css';
 
 function GuessBoxes() {
+  const [showCityList, setShowCityList] = useState(false);
+  const inputRef = useRef(null);
+  const suggestionRef = useRef(null);
+
+  const handleInputFocus = () => {
+    
+    setShowCityList(true);
+  };
+
+  const handleCitySelection = (city) => {
+    
+    document.getElementById("text-field").value = city;
+    setShowCityList(false)
+    // console.log(`Selected city: ${city}`);
+  };
+
+  const handleClickOutside = (event) => {
+    if (
+      inputRef.current && !inputRef.current.contains(event.target) &&
+      suggestionRef.current && !suggestionRef.current.contains(event.target)
+    ) {
+      setShowCityList(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('click', handleClickOutside);
+    return () => {
+      window.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="guess-boxes">
+      {showCityList && (
+        <div className="suggestion-buttons" ref={suggestionRef}>
+          {capitalCities.map((city, index) => (
+            <button
+              key={index}
+              className="suggestion-button"
+              onClick={() => handleCitySelection(city.city)}
+            >
+              {city.city}
+            </button>
+          ))}
+        </div>
+      )}
       {[...Array(6)].map((_, index) => (
         <div key={index} className="rectangle"></div>
       ))}
-      <input className="text-field" type="text" placeholder='City, Country, territory... '/>
-      <button className="guess-button"> 
+      <input
+        id='text-field'
+        className="text-field"
+        type="text"
+        placeholder="City, Country, territory..."
+        onFocus={handleInputFocus}
+        ref={inputRef}
+      />
+      <button className="guess-button">
         Guess! 🌇
       </button>
     </div>
--- src/components/GuessButton.jsx
+++
@@ -1,8 +0,0 @@
-// import React from 'react';
-// import '../css/GuessButton.css'
-
-// function GuessButton() {
-//   return <button className="guess-button">Guess</button>;
-// }
-
-// export default GuessButton;
